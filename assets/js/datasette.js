f5939f903125d6fbbc4bd08bd3f28b383571f826
Alex Bilson <acbilson@gmail.com>
2020-11-16 08:11:28 -0600

    hides memorial when no data exists

--- assets/js/datasette.js
+++ assets/js/datasette.js
@@ -3,22 +3,26 @@ window.addEventListener('load', (event) => {
 });
 
 function getCovidDeaths() {
-  const url = 'https://data.alexbilson.dev/medical_examiner_case_archive__covid19_related_deaths.json?sql=select%0D%0A++death_date%2C%0D%0A++age%2C%0D%0A++gender%2C%0D%0A++race%2C%0D%0A++primarycause%2C%0D%0A++incident_street%2C%0D%0A++incident_city%0D%0Afrom%0D%0A++medical_examiner_case_archive__covid19_related_deaths%0D%0Awhere%0D%0A++death_date+%3E%3D+date%28%27now%27%2C+%27-1+days%27%29%0D%0Aorder+by%0D%0A++death_date+desc';
+  const url = 'https://data.alexbilson.dev/medical_examiner_case_archive__covid19_related_deaths.json?sql=select%0D%0A++death_date%2C%0D%0A++age%2C%0D%0A++latino%2C%0D%0A++gender%2C%0D%0A++race%2C%0D%0A++primarycause%2C%0D%0A++incident_street%2C%0D%0A++incident_city%0D%0Afrom%0D%0A++medical_examiner_case_archive__covid19_related_deaths%0D%0Awhere%0D%0A++death_date+%3E%3D+date%28%27now%27%2C+%27-1+days%27%29%0D%0Aorder+by%0D%0A++death_date+desc';
 
   const rowIndex = {
     death_date: 0,
     age: 1,
-    gender: 2,
-    race: 3,
-    primarycause: 4,
-    incident_street: 5,
-    incident_city: 6
+    latino: 2,
+    gender: 3,
+    race: 4,
+    primarycause: 5,
+    incident_street: 6,
+    incident_city: 7
   };
+
+  var hasData = false;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
 
+      hasData = data.rows.length > 0;
       var el = document.getElementById('deaths');
 
       // create list
@@ -31,7 +35,7 @@ function getCovidDeaths() {
 
         var age = r[rowIndex.age];
         var gender = getGender(r[rowIndex.gender]);
-        var race = getRace(r[rowIndex.race]);
+        var race = getRace(r[rowIndex.race], r[rowIndex.latino]);
         var cause = getCause(r[rowIndex.primarycause]);
         var address = getAddress(r[rowIndex.incident_street], r[rowIndex.incident_city]);
 
@@ -42,7 +46,9 @@ a ${age}-year-old ${race} ${gender} ${cause}${address}.
         el.appendChild(item);
       });
 
-      document.getElementById('covid').style = 'display: inherit;';
+      if (hasData) {
+        document.getElementById('covid').style = 'display: inherit;';
+      }
     });
 }
 
@@ -63,11 +69,12 @@ function getGender(val) {
   }
 }
 
-function getRace(val) {
+function getRace(val, latino) {
   var race = val.toLowerCase().trim();
   if (['other', 'unknown'].includes(race)) {
     return '';
   }
+  if (latino) { return 'latino'; }
   return race;
 }
 
