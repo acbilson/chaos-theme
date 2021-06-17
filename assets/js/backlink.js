589285f27013e0a50984d10a89973609420a0029
Alex Bilson <acbilson@gmail.com>
2021-06-17 15:41:27 -0500

    appends popup as child to internal links, then shows them with css

--- assets/js/backlink.js
+++ assets/js/backlink.js
@@ -1,118 +1,56 @@
-// TODO: find a better way to populate the link
-//       preview than query after each hover
-//       It breaks after too many hovers and should
-//       probably only be made visible by hovering.
-
-// I'm thinking that we should turn all internal links into a content array
-// available for a number of methods in state. The content divs could be placed
-// anywhere on the page since they're absolute, and I may consider using
-// a similar approach to fill backlink preview context instead of manually
-// including the context in the metadata.
-
-// THOUGHT: might be worth wiring up the babel transpiler so I can use ES2016
-
-class BacklinkGenerator {
-
-  constructor(links) {
-    this.links = {};
-    this.parser = new DOMParser();
-    this.getLinkContent(links);
-  }
-
-  getLinkContent(links) {
-    links.forEach((link) => {
-
-      fetch(link.href)
-        .then((resp) => {
-        if (resp.status === 200) {
-          return resp.text();
-        } else {
-          return null;
-        }})
-        .then((text) => {
-          if (text === null) { return null; }
-          const contentElement = this.parsePageContent(text);
-          this.links[link.href] = contentElement;
-          console.log(this.links);
-      });
-    });
-  }
-
-  parsePageContent(text) {
-    var bodyDOM = this.parser.parseFromString(text, 'text/html');
-    const contentElement = bodyDOM.querySelector('div.e-content');
-
-    if (contentElement) {
-      return contentElement.firstElementChild;
-    } else {
-      const missing = document.createElement('p');
-      missing.text = 'no content found';
-      return missing;
-    }
-  }
-}
-
-function createPopup(offsetTop, offsetLeft, contentElement) {
-
-  const popup = document.createElement('div');
-  popup.id = 'popup';
-  popup.classList.add('backlink-popup');
-  popup.style.top = `${offsetTop + 20}px`;
-  popup.style.left = `${offsetLeft}px`;
-  popup.appendChild(contentElement);
-
-  return popup;
-}
-
 function parsePageContent(text) {
-
   var bodyDOM = new DOMParser().parseFromString(text, 'text/html');
   const contentElement = bodyDOM.querySelector('div.e-content');
 
   if (contentElement) {
-    return contentElement.firstElementChild;
+    const elementCount = contentElement.childElementCount < 3 ? contentElement.childElementCount : 3;
+    const contentSlice = Array.from(contentElement.children).slice(0, elementCount);
+
+    if (elementCount < contentElement.childElementCount) {
+      const ellipsisElement = document.createElement('p');
+      ellipsisElement.innerText = '...';
+      contentSlice.push(ellipsisElement);
+    }
+    return contentSlice;
+
   } else {
     const missing = document.createElement('p');
     missing.text = 'no content found';
-    return missing;
+    return [missing];
   }
 }
 
-function showBacklink(event) {
+function createPopup(offsetTop, offsetLeft, contentElements) {
+  const popup = document.createElement('div');
 
-  const link = event.target;
+  popup.classList.add('backlink-popup', 'hide');
+  popup.style.top = `${offsetTop + 20}px`;
+  popup.style.left = `${offsetLeft}px`;
+  contentElements.forEach((el) => popup.appendChild(el));
 
-  fetch(link.href).then(function(resp) {
-
-    if (resp.status === 200) {
-      return resp.text();
-    } else {
-      return null;
-    }
-  }).then(function(text) {
-    if (text === null) { console.log('no content found'); }
-    const contentElement = parsePageContent(text);
-    const popup = createPopup(link.offsetTop, link.offsetLeft, contentElement);
-    document.body.appendChild(popup);
-  });
+  return popup;
 }
 
-function hideBacklink(event) {
-  const popup = document.getElementById('popup');
-  document.body.removeChild(popup);
-}
-
-function newShowBacklink(event) {
-  const content = document.generator.links[event.target.href];
-  console.log(content.innerHTML);
+function appendLinkPopup(link) {
+  fetch(link.href)
+    .then((resp) => {
+      if (resp.status === 200) {
+        return resp.text();
+      } else {
+        return null;
+      }
+    })
+    .then((text) => {
+      if (text === null) { return null; }
+      const contentElements = this.parsePageContent(text);
+      const popupElement = createPopup(link.offsetTop, link.offsetLeft, contentElements);
+      link.appendChild(popupElement);
+    });
 }
 
 // adds backlink events to all links marked internal
 const internalLinks = document.querySelectorAll('a.internal');
 
-internalLinks.forEach(function(link) {
-  link.addEventListener('mouseover', newShowBacklink);
-  link.addEventListener('mouseout', hideBacklink);
+internalLinks.forEach((link) => {
+  appendLinkPopup(link);
 });
-
-document.generator = new BacklinkGenerator(internalLinks);
