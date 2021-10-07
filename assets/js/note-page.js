b86487f0cf9a485ece40787e23bdf01dffe81310
Alex Bilson <acbilson@gmail.com>
2021-10-07 07:23:11 -0500

    combines into one module b/c import fails - the import call does not recognize the content type and the browser won't allow text/plain

--- assets/js/note-page.js
+++ assets/js/note-page.js
@@ -1,6 +1,39 @@
-import { NoteForm, NoteCard } from '/js/components/note-form.js';
-
 document.addEventListener('DOMContentLoaded', (e) => {
+    class NoteCard {
+
+        get epistemic() {
+            return this.el.dataset.epistemic;
+        }
+
+        constructor(el) {
+            this.el = el;
+        }
+
+        selectEpistemic(selectedEpistemic) {
+            this.el.style.display = this.epistemic === selectedEpistemic ? 'block' : 'none';
+        }
+    }
+
+    class NoteForm {
+
+        get selectedEpistemic() {
+            return Array.from(this.form.epistemicSelector.selectedOptions).map(o => o.value)[0];
+        }
+
+        constructor(form, notes) {
+            this.form = form;
+            this.notes = notes;
+        }
+
+        handleForm() {
+            this.filterByEpistemic();
+        }
+
+        filterByEpistemic() {
+            Array.from(this.notes).forEach(n => n.selectEpistemic(this.selectedEpistemic));
+        }
+    }
+
     const form = document.notesForm;
     const notes = Array.from(document
             .getElementById('notes-list')
@@ -16,7 +49,6 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
     document.notesForm.addEventListener('submit', (e) => {
         e.preventDefault(); // refreshes page w/o this
-        console.log(noteForm);
         noteForm.handleForm();
     });
 });
