a2229f7fd31c95f66d2b7c385553e515cab3f64c
Alex Bilson <acbilson@gmail.com>
2022-06-09 10:46:19 -0500

    formats js/css with prettier

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,7 +1,9 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-	const routePage = this.location.pathname.slice(1).split('/')[0];
-	const navDropdown = document.getElementById('nav-router');
-	const optionToSelect = Array.from(navDropdown.children).find(l => l.value === routePage);
+document.addEventListener("DOMContentLoaded", (e) => {
+	const routePage = this.location.pathname.slice(1).split("/")[0];
+	const navDropdown = document.getElementById("nav-router");
+	const optionToSelect = Array.from(navDropdown.children).find(
+		(l) => l.value === routePage
+	);
 	if (optionToSelect) {
 		optionToSelect.selected = true;
 	}
