bca052797b240b7fe03ce0072bf134cff2f4e254
Alex Bilson <acbilson@gmail.com>
2022-06-14 12:39:15 -0500

    js cleanup

--- assets/js/nav-bar.js
+++ assets/js/nav-bar.js
@@ -1,32 +1,27 @@
+function hide(e) {
+	document.querySelector("section").classList.add("hide");
+
+	const menuEls = document.getElementById("navbar").querySelectorAll("div");
+
+	Array.from(menuEls).map((el) => el.classList.add("reveal"));
+
+	e.target.innerText = "HIDE";
+}
+
+function reveal(e) {
+	document.querySelector("section").classList.remove("hide");
+
+	const menuEls = document.getElementById("navbar").querySelectorAll("div");
+
+	Array.from(menuEls).map((el) => el.classList.remove("reveal"));
+
+	e.target.innerText = "MENU";
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
-	const routePage = this.location.pathname.slice(1).split("/")[0];
-	const navDropdown = document.getElementById("nav-router");
-	const optionToSelect = Array.from(navDropdown.children).find(
-		(l) => l.value === routePage
+	const buttonEl = document.getElementById("navbar").querySelector("button");
+
+	buttonEl.addEventListener("click", (e) =>
+		e.target.innerText === "MENU" ? hide(e) : reveal(e)
 	);
-	if (optionToSelect) {
-		optionToSelect.selected = true;
-	}
-});
-
-const buttonEl = document.getElementById("navbar").querySelector("button");
-
-buttonEl.addEventListener("click", (e) => {
-	if (e.target.innerText === "MENU") {
-		document.querySelector("section").classList.add("hide");
-
-		const menuEls = document.getElementById("navbar").querySelectorAll("div");
-
-		Array.from(menuEls).map((el) => el.classList.add("reveal"));
-
-		e.target.innerText = "HIDE";
-	} else {
-		document.querySelector("section").classList.remove("hide");
-
-		const menuEls = document.getElementById("navbar").querySelectorAll("div");
-
-		Array.from(menuEls).map((el) => el.classList.remove("reveal"));
-
-		e.target.innerText = "MENU";
-	}
 });
