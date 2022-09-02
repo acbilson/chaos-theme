605bb1392d0891e4e7116cbea62d19d833d865e3
Alex Bilson <acbilson@gmail.com>
2022-09-02 08:32:31 -0600

    starts a backlink web component

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,15 +1,69 @@
-import {LitElement, html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { LitElement, PropertyValues, html, css } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 
-@customElement('my-element')
+@customElement("my-element")
 export class MyElement extends LitElement {
-  @property()
-  version = 'STARTING';
+	@property()
+	version = "STARTING";
 
-  render() {
-    return html`
-    <p>Welcome to the Lit tutorial!</p>
-    <p>This is the ${this.version} code.</p>
-    `;
-  }
+	render() {
+		return html`
+			<p>Welcome to the Lit tutorial!</p>
+			<p>This is the ${this.version} code.</p>
+		`;
+	}
+}
+
+@customElement("app-backlink")
+export class Backlink extends LitElement {
+	@property()
+	href: string;
+
+	@property()
+	paragraph = 3;
+
+	@state()
+	private _name = 'temp';
+
+	static elementTypes: string[] = [
+		"h1",
+		"h2",
+		"h3",
+		"h4", // headers
+		"p", // paragraphs
+		"ol",
+		"ul",
+		"li", // lists
+	];
+
+	static styles = css`
+		.backref {
+			text-transform: capitalize;
+			text-decoration-line: underline;
+			text-decoration-style: double;
+		}
+	`;
+
+	constructor() {
+		super();
+		this.addEventListener('mouseover', () => console.log('hover-mode'));
+	}
+
+	updated(prevProps: PropertyValues<this>) {
+		if (prevProps.has('href')) {
+			const urlParts = this.href.split("/");
+			this._name = urlParts[urlParts.length - 2];
+		}
+	}
+
+	render() {
+		const urlParts = this.href.split("/");
+		const name = urlParts[urlParts.length - 2];
+		return html`
+		<a
+			class="backref"
+			href="${ this.href }"
+		>${ this._name }</a>
+		`;
+	}
 }
