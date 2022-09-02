67ca02087d9e0ddd60f1d6eeffa2c9d7234e7ac2
Alex Bilson <acbilson@gmail.com>
2022-09-02 13:45:17 -0600

    makes a basic panel and aside

--- app/src/backlink.ts
+++ app/src/backlink.ts
@@ -7,23 +7,10 @@ export class Backlink extends LitElement {
 	href: string;
 
 	@property()
+	title: string;
+
+	@property()
 	paragraph: number;
-
-	@state()
-	private _name = 'temp';
-
-	private _parser = null;
-
-	static elementTypes: string[] = [
-		"h1",
-		"h2",
-		"h3",
-		"h4", // headers
-		"p", // paragraphs
-		"ol",
-		"ul",
-		"li", // lists
-	];
 
 	static styles = css`
 		.backref {
@@ -33,52 +20,28 @@ export class Backlink extends LitElement {
 		}
 	`;
 
-	_filterElements(elements: Array<HTMLElement>, elementTypes) {
-		if (!elements || elements.length < 1) return null;
-		const validElements = Array.from(elements).filter((el) =>
-			el ? elementTypes.includes(el.localName) : false
-		);
-		const startIndex = this.paragraph ?? 0;
-		const endIndex =
-			validElements.length - startIndex > 3
-				? startIndex + 3
-				: validElements.length;
-		return Array.from(validElements).slice(startIndex, endIndex);
-	}
-
-	_getPopupContent(href: string) {
-		fetch(this.href)
-			.then((r) => (r.status === 200 ? r.text() : null))
-			.then((t) => {
-				if (t === null) return null;
-				const pageDOM = this._parser.parseFromString(t, "text/html");
-				const contentElement = pageDOM.querySelector(".e-content");
-				const filtered = this._filterElements(contentElement.children, Backlink.elementTypes);
-				console.log({content: contentElement, filtered: filtered});
-			});
-	}
-
 	constructor() {
 		super();
-		this._parser = new DOMParser();
-		this.addEventListener('mouseover', () => this._getPopupContent(this.href));
+		this.addEventListener('click', () => this.backlinkClicked());
 	}
 
-	updated(prevProps: PropertyValues<this>) {
-		if (prevProps.has('href')) {
-			const urlParts = this.href.split("/");
-			this._name = urlParts[urlParts.length - 2];
-		}
+	backlinkClicked() {
+		console.log("dispatching");
+		this.dispatchEvent(
+			new CustomEvent('backlink-clicked', {
+			detail: {
+				href: this.href
+			},
+			bubbles: true,
+			composed: true
+		}));
 	}
 
 	render() {
-		const urlParts = this.href.split("/");
-		const name = urlParts[urlParts.length - 2];
 		return html`
 		<a
 			class="backref"
-			href="${ this.href }"
-		>${ this._name }</a>
+		>${ this.title }</a>
 		`;
 	}
 }
--- app/src/panel.ts
+++ app/src/panel.ts
@@ -3,18 +3,8 @@ import { customElement, property, state } from "lit/decorators.js";
 
 @customElement("app-panel")
 export class Panel extends LitElement {
-	@property()
-	href: string;
-
-	constructor() {
-		super();
-		// TODO: listen for a custom backlink event
-		this.addEventListener('mouseover', (e) => console.log(e));
-	}
-
 	static styles = css`
 		.panel {
-			width: 50%;
 		}
 	`;
 
@@ -29,15 +19,38 @@ export class Panel extends LitElement {
 
 @customElement("app-panels")
 export class Panels extends LitElement {
+	private _parser = new DOMParser();
+
+	@state()
+	asideContent: HTMLElement = null;
+
+	private getContent(href: string) {
+		fetch(href)
+			.then((r) => (r.status === 200 ? r.text() : null))
+			.then((t) => {
+				const pageDOM = this._parser.parseFromString(t, "text/html");
+				this.asideContent = pageDOM.querySelector(".e-content");
+			});
+			console.log({url: href, aside: this.asideContent});
+	}
+
+	constructor() {
+		super();
+		this.addEventListener('backlink-clicked', (e: CustomEvent) => this.getContent(e.detail.href));
+	}
+
 	static styles = css`
-		.panels { }
+		.panels {
+			display: flex;
+			flex-flow: row nowrap;
+		}
 	`;
 
 	render() {
 		return html`
 		<div class="panels">
-			<slot name="main"></slot>
-			<slot name="aside"></slot>
+			<app-panel><slot name="main"></slot></app-panel>
+			<app-panel>${ this.asideContent }<slot name="aside"></slot></app-panel>
 		</div>
 		`;
 	}
