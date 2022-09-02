3c8eb6a35d1118de4fbcfe975dd38efc5ae84e92
Alex Bilson <acbilson@gmail.com>
2022-09-02 08:55:17 -0600

    adds content filter to backlink hover

--- app/src/app.ts
+++ app/src/app.ts
@@ -20,10 +20,12 @@ export class Backlink extends LitElement {
 	href: string;
 
 	@property()
-	paragraph = 3;
+	paragraph: number;
 
 	@state()
 	private _name = 'temp';
+
+	private _parser = null;
 
 	static elementTypes: string[] = [
 		"h1",
@@ -44,9 +46,35 @@ export class Backlink extends LitElement {
 		}
 	`;
 
+	_filterElements(elements: Array<HTMLElement>, elementTypes) {
+		if (!elements || elements.length < 1) return null;
+		const validElements = Array.from(elements).filter((el) =>
+			el ? elementTypes.includes(el.localName) : false
+		);
+		const startIndex = this.paragraph ?? 0;
+		const endIndex =
+			validElements.length - startIndex > 3
+				? startIndex + 3
+				: validElements.length;
+		return Array.from(validElements).slice(startIndex, endIndex);
+	}
+
+	_getPopupContent(href: string) {
+		fetch(this.href)
+			.then((r) => (r.status === 200 ? r.text() : null))
+			.then((t) => {
+				if (t === null) return null;
+				const pageDOM = this._parser.parseFromString(t, "text/html");
+				const contentElement = pageDOM.querySelector(".e-content");
+				const filtered = this._filterElements(contentElement.children, Backlink.elementTypes);
+				console.log({content: contentElement, filtered: filtered});
+			});
+	}
+
 	constructor() {
 		super();
-		this.addEventListener('mouseover', () => console.log('hover-mode'));
+		this._parser = new DOMParser();
+		this.addEventListener('mouseover', () => this._getPopupContent(this.href));
 	}
 
 	updated(prevProps: PropertyValues<this>) {
