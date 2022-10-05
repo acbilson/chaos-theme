f8b8a5f0a3e1c6c837787f7c8e2fd22ba11be30c
Alex Bilson <acbilson@gmail.com>
2022-10-05 14:11:20 -0600

    fixes missing auth header

--- app/components/file/publish-controller.ts
+++ app/components/file/publish-controller.ts
@@ -20,13 +20,21 @@ export class PublishController implements ReactiveController {
 		token: string,
 		payload: ChangeResult
 	): Promise<Response<ChangeResult>> {
-		if (token == null) new Promise(() => false);
+		if (token == null)
+			new Promise(
+				() =>
+					<Response<ChangeResult>>{
+						success: false,
+						message: "no token retrieved for update",
+					}
+			);
 		const headers = new Headers();
 		headers.append("Authorization", `Bearer ${token}`);
+		headers.append("Content-Type", "application/json; charset=UTF-8");
 		return fetch(new URL("file", BaseUrls.publish), {
 			method: "PUT",
 			body: JSON.stringify(payload),
-			headers: { "Content-Type": "application/json; charset=UTF-8" },
+			headers,
 		})
 			.then((r) => (r.status === 200 ? r.json() : null))
 			.then((b) => <Response<ChangeResult>>b);
@@ -36,13 +44,21 @@ export class PublishController implements ReactiveController {
 		token: string,
 		payload: ChangeResult
 	): Promise<Response<ChangeResult>> {
-		if (token == null) new Promise(() => false);
+		if (token == null)
+			new Promise(
+				() =>
+					<Response<ChangeResult>>{
+						success: false,
+						message: "no token retrieved for update",
+					}
+			);
 		const headers = new Headers();
 		headers.append("Authorization", `Bearer ${token}`);
+		headers.append("Content-Type", "application/json; charset=UTF-8");
 		return fetch(new URL("file", BaseUrls.publish), {
 			method: "POST",
 			body: JSON.stringify(payload),
-			headers: { "Content-Type": "application/json; charset=UTF-8" },
+			headers,
 		})
 			.then((r) => (r.status === 200 ? r.json() : null))
 			.then((b) => <Response<ChangeResult>>b);
