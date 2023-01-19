38ece9a2eb98d9d847a99bc0aac2b3075c1df14c
Alex Bilson <acbilson@gmail.com>
2023-01-19 15:47:52 -0700

    loads lunr index from new index service

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,10 +3,11 @@ function displayResults(results, store) {
 	if (results.length) {
 		let resultList = "";
 		// Iterate and build result list elements
-		for (const n in results) {
-			const item = store[results[n].ref];
+		for (const i in results) {
+			const id = results[i].ref;
+			const item = store.find(x => x.id === id);
 			resultList +=
-				'<li><p><a href="' + item.url + '">' + item.title + "</a></p>";
+				'<li><p><span style="padding-right: 9px;">' + item.author + '</span><a href="' + item.id + '">' + item.title + "</a></p>";
 			resultList += "<p>" + item.content.substring(0, 100) + "...</p></li>";
 		}
 		searchResults.innerHTML = resultList;
@@ -24,6 +25,7 @@ if (query) {
 	// Retain the search input in the form when displaying results
 	document.getElementById("search-input").setAttribute("value", query);
 
+	/*
 	const idx = lunr(function () {
 		this.ref("id");
 		this.field("title", {
@@ -43,9 +45,78 @@ if (query) {
 			});
 		}
 	});
+	*/
 
-	// Perform the search
-	const results = idx.search(query);
-	// Update the list with results
-	displayResults(results, window.store);
+	/*
+
+	parser = new DOMParser();
+	fetch('/json/index.html')
+		.then((r) => (r.status === 200 ? r.text() : ""))
+		.then((r) => (parser.parseFromString(r, 'text/html')))
+		.then(r => {
+			const data = JSON.parse(r.body.innerText);
+			console.log({lunr, data});
+
+			const idx = lunr(function () {
+				this.ref("id");
+				this.field("title", {
+					boost: 15,
+				});
+				//this.field("tags");
+				this.field("content", {
+					boost: 10,
+				});
+
+				for (var i in data) {
+					this.add({
+						id: data[i].id,
+						title: data[i].title,
+						//tags: window.store[key].category,
+						content: data[i].content,
+					});
+				}
+			});
+
+			//lunr.Index.load(data);
+
+			// Perform the search
+			const results = idx.search(query);
+			console.log({index: data, idx, query, results});
+			// Update the list with results
+			displayResults(results, data);
+		});
+		*/
+
+	fetch('https://index.alexbilson.dev/index')
+		.then((r) => (r.status === 200 ? r.json() : ""))
+		.then((data) => {
+
+			const idx = lunr(function () {
+				this.ref("id");
+				this.field("title", {
+					boost: 15,
+				});
+				//this.field("tags");
+				this.field("content", {
+					boost: 10,
+				});
+
+				for (var i in data) {
+					this.add({
+						id: data[i].id,
+						title: data[i].title,
+						author: data[i].author,
+						content: data[i].content,
+					});
+				}
+			});
+
+			// Perform the search
+			const results = idx.search(query);
+			console.log({index: data, idx, query, results});
+			// Update the list with results
+			displayResults(results, data);
+
+		});
+
 }
